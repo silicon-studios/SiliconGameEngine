f9d903e78d0824559841daafd126cff2cf87884a
silicondev <josh@siliconstudios.co.uk>
2021-08-21 17:10:34 +0100

    Add project files.

---
+++ Objects/GameObject.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SiliconGameEngine.PhysicsLogic;
+using System.Text;
+using System.Threading.Tasks;
+using SiliconGameEngine.Textures;
+
+namespace SiliconGameEngine.Objects
+{
+    public abstract class GameObject
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public Vector2 Location { get; set; }
+        public Physics Physics { get; set; }
+        public Vector2 NextLocation => Location.GetMove(Physics.Velocity);
+        public Vector2 Size
+        {
+            get
+            {
+                if (_size == null)
+                {
+                    Texture2D tex = Sprite;
+                    if (_frames.Any())
+                        tex = _frames[0];
+                    _size = new Vector2(tex.Width, tex.Height);
+                }
+
+                return _size.Value;
+            }
+        }
+        public Bounds Bounds => new Bounds(Location, Size);
+        public Bounds NextBounds => new Bounds(NextLocation, Size);
+        private Vector2? _size = null;
+        public DateTime FallTime { get; set; }
+        public ObjectId ObjectId { get; } = ObjectId.EMPTY;
+        public Texture2D Sprite { get; private set; }
+        public float Scale { get; set; } = 2f;
+        public float Rotation { get; set; } = 0f;
+        private List<Texture2D> _frames = new List<Texture2D>();
+        protected bool Usable { get; private set; } = false;
+
+        public GameObject(ObjectId id)
+        {
+            ObjectId = id;
+            Id = Guid.NewGuid();
+        }
+
+        public void Create(string name, Vector2 loc, Texture2D sprite, float scale = 2f, float rotation = 0f)
+        {
+            Location = loc;
+            Sprite = sprite;
+            Name = name;
+
+            Scale = scale;
+            Rotation = rotation;
+
+            if (ObjectId != ObjectId.EMPTY && Id != null)
+                Usable = true;
+        }
+
+        public Texture2D GetFrame(GraphicsDevice device, int i)
+        {
+            if (_frames.Count == 0)
+            {
+                _frames = Sprite.GetSprites(device, 16);
+            }
+            return _frames[i];
+        }
+
+        public void IteratePhysics()
+        {
+            Physics.Iterate();
+            Location.Move(Physics.Velocity);
+        }
+    }
+
+    public enum ObjectId
+    {
+        EMPTY,
+        ENTITY_PLAYER,
+        ENTITY_ENEMY_ZOMBIE,
+
+        TILE_GRASS
+    }
+
+    public class Bounds
+    {
+        public float T { get; }
+        public float B { get; }
+        public float L { get; }
+        public float R { get; }
+
+        public Bounds(Vector2 pos, Vector2 size)
+        {
+            T = pos.Y;
+            B = pos.Y + size.Y;
+            L = pos.X;
+            R = pos.X + size.X;
+        }
+
+        public bool Intersect(Bounds other) => Intersect(this, other);
+
+        public static bool Intersect(Bounds a, Bounds b)
+        {
+            bool at = a.T >= b.T && a.T <= b.B;
+            bool ab = a.B >= b.T && a.B <= b.B;
+            bool al = a.L >= b.L && a.L <= b.R;
+            bool ar = a.R >= b.L && a.R <= b.R;
+            return (at || ab) && (al || ar);
+        }
+    }
+}
---
+++ PhysicsLogic/Physics.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SiliconGameEngine.PhysicsLogic
+{
+    public class Physics
+    {
+        public bool IsFalling { get; private set; } = false;
+        public bool IsMoving => Velocity.X != 0 || Velocity.Y != 0;
+        public Vector2 Velocity { get; set; }
+
+        public List<Exertion> Exertions { get; } = new List<Exertion>()
+        {
+            new Exertion
+            {
+                Name = "exertion:gravity",
+                Constant = true,
+                Value = new Vector2(0, g),
+                Torque = 0
+            }
+        };
+
+        public const float g = 4f;
+        public const float t = 10f;
+
+        public void Iterate()
+        {
+
+            //foreach (var obj in LoadedObjects)
+            //{
+            //    bool canMove = true;
+
+            //    if (obj.IsFalling)
+            //        Gravity(obj);
+
+            //    Vector2 newMove = new Vector2(obj.Location.X + obj.Velocity.X, obj.Location.Y + obj.Velocity.Y);
+            //    Bounds newBounds = new Bounds(newMove, obj.Size);
+
+            //    foreach (var obj2 in LoadedObjects.Where(x => Pythag(Dist(obj.Location.X, x.Location.X), Dist(obj.Location.Y, x.Location.Y)) < 100))
+            //    {
+            //        if (Collide(obj, obj2))
+            //        {
+            //            canMove = false;
+            //            break;
+            //        }
+            //    }
+
+            //    if (canMove)
+            //        obj.Move();
+            //}
+
+
+            foreach (var force in new List<Exertion>(Exertions))
+            {
+                //Velocity.X += force.Value.X;
+                //Velocity.Y += force.Value.Y;
+                Vector2 val = new Vector2((force.Value.X / 100) * force.Torque, (force.Value.Y / 100) * force.Torque);
+                Velocity.Move(val);
+
+                force.Torque += t;
+
+                if (!force.Constant && force.Torque >= 100f)
+                    Exertions.Remove(force);
+            }
+
+
+
+        }
+
+        public static float Dist(float a, float b) => a > b ? a - b : b > a ? b - a : 0f;
+        public static float Pythag(float x, float y) => (float)Math.Sqrt((x * x) + (y * y));
+
+        //private static void Gravity()
+        //{
+        //    float v = Velocity.Y;
+        //    float t = (DateTime.Now.Ticks - obj.FallTime.Ticks) / 10000000f;
+
+        //    float nv = (float)(0.5f * g * (t * t)) + v;
+
+        //    obj.AccelY(nv);
+        //}
+    }
+
+    public class Exertion
+    {
+        public string Name { get; set; }
+        public Vector2 Value { get; set; }
+        public float Torque { get; set; } = 100f;
+        public bool Constant { get; set; } = false;
+    }
+
+    public static class PhysicsExtensions
+    {
+        public static void Move(this Vector2 vector, Vector2 value)
+        {
+            vector = vector.GetMove(value);
+        }
+
+        public static Vector2 GetMove(this Vector2 vector, Vector2 value)
+        {
+            float newX = vector.X + value.X;
+            float newY = vector.Y + value.Y;
+            return new Vector2(newX, newY);
+        }
+    }
+}
